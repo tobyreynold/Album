dabd0c1e46b61bf5e5c04a245d604b5c23520a02
liuye03 <liuye03@meituan.com>
2016-03-23 23:07:27 +0800

    add part

--- album/public/javascripts/album.js
+++ album/public/javascripts/album.js
@@ -21,6 +21,7 @@
 			}
 		}
 
+		/*pulic attr*/
 		_this.dom = typeof _this.options.dom ==='string'? $(_this.options.dom) : _this.options.dom;
 		_this.time = _this.options.time || 3000;
 		_this.isVertical = _this.options.isVertical;
@@ -31,6 +32,11 @@
 		_this.autoPlay = _this.options.autoPlay;
 		_this.data = Object.prototype.toString.call(_this.options.data) === '[object Array]'? _this.options.data : [];
 
+		/*private attr*/
+		_this.animated = false;
+		_this.len = _this.data.length;
+		_this.width = _this.dom.width();
+
 		_this.renderHtml();
 		_this.init();
 		_this.handler();
@@ -38,12 +44,23 @@
 	}
 
 	album.prototype.handler = function() {
+		var _this = this;
+
+		_this.dom.on('click',_this.options.prev,function() {
+			_this.prvSlide();
+		});
+
+		_this.dom.on('click',_this.options.next,function() {
+			_this.nextSlide();
+		});
 		
 	}
 
 	album.prototype.renderHtml = function() {
 		var _this = this,
 			outer = $("<ul></ul>"),
+			prev = $("<span class='prev'></span>"),
+			next = $("<span class='next'></span>"),
 			len = this.data.length,
 			cont = '';
 
@@ -60,7 +77,7 @@
 		}
 		
 		$(outer).append(cont);
-		$(_this.dom).append(outer);
+		$(_this.dom).append(outer).append(prev).append(next);
 	}
 
 	album.prototype.init = function() {
@@ -68,10 +85,37 @@
 	}
 
 	album.prototype.nextSlide = function() {
-		console.log(1);
+		this.swithTo(this.index + 1);
+	}
+
+	album.prototype.prvSlide = function() {
+		this.swithTo(this.index - 1);
 	}
 
 	album.prototype.swithTo = function(index) {
+		var _this = this;
+
+		if(_this.index == index) return;
+
+		var newIndex = index > _this.len ? 1 : (index < 1 ? _this.len : index);
+		_this.index = newIndex;
+
+		//
+		_this.setPosition(-index * _this.width,newIndex);
+
+	}
+
+
+	album.prototype.setPosition = function(pos,index,hasAnimate) {
+
+		var _this = this,
+		    $eles = _this.dom.find("li"),
+		    $currentDom = _this.dom.find("li.current");
+
+		    if(!hasAnimate) {
+		    	$currentDom.removeClass("current").addClass("hidden");
+		    	$eles.eq(index-1).removeClass("hidden").addClass("current");
+		    }
 
 	}
 
@@ -91,8 +135,6 @@
 				_this.nextSlide();
 			},_this.time);	
 		}
-		
-	
 	}
 
 
